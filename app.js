d8bc715efe6f6703a253f2c97e7d9eb811b1aca6
pedrodinia <132511767+pedrodinia@users.noreply.github.com>
2023-05-24 23:17:39 -0300

    MercadoLiebrePAD

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require ("express");
+const path = require ("path")
+const app = express ();
+
+app.use(express.static('public'));
+
+app.listen (process.env.PORT || 3000, () => {
+    console.log ("servidor corriendo en el puerto 3000")
+})
+
+app.get('/', (req, res)  => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get('/login', (req, res)  => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+app.get('/register', (req, res)  => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
